0442dbfece84b5730350b5cabf3599f60822e1bf
marcin <marcin7848@gmail.com>
2021-06-28 20:01:39 +0200

    load all necessary data for checkQueueThread

--- backend/src/main/java/com/gfnqueuechecker/backend/ProcessThreads.java
+++ backend/src/main/java/com/gfnqueuechecker/backend/ProcessThreads.java
@@ -10,11 +10,11 @@ public class ProcessThreads {
         Class.forName("org.h2.Driver");
         connection = DriverManager
                 .getConnection("jdbc:h2:file:./database;DB_CLOSE_ON_EXIT=FALSE;AUTO_SERVER=TRUE",
-                        "dbuser","pass@");
+                        "dbuser", "pass@");
     }
 
 
-    public static void lastSearchedThread(){
+    public static void lastSearchedThread() {
         try {
             Statement stmt = connection.createStatement();
             String sql = "delete from LAST_SEARCHED where LAST_SEARCHED_ID NOT IN " +
@@ -36,7 +36,60 @@ public class ProcessThreads {
         );
     }
 
-    public static void checkQueueThread(){
-        System.out.println("x");
+    public static void checkQueueThread() {
+        try {
+            //TODO: delete to old
+
+            Statement stmt = connection.createStatement();
+            String sql = "select * from CHECK_QUEUE WHERE PROCESS = 0 OR PROCESS = 1 order by ADD_TIME limit 1";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            if (!rs.isBeforeFirst()) {
+                new java.util.Timer().schedule(
+                        new java.util.TimerTask() {
+                            @Override
+                            public void run() {
+                                checkQueueThread();
+                            }
+                        },
+                        3000
+                );
+                return;
+            }
+
+            rs.next();
+            long id = rs.getLong("CHECK_QUEUE_ID");
+            long gameId = rs.getLong("GAME_ID");
+            long serverId = rs.getLong("SERVER_ID");
+
+            sql = "select * from CONFIG where CONFIG_NAME = 'Authorization'";
+            ResultSet rs2 = stmt.executeQuery(sql);
+            rs2.next();
+            String authorization = rs2.getString("CONFIG_VALUE");
+
+            sql = "select * from CONFIG where CONFIG_NAME = 'X-Device-Id'";
+            ResultSet rs3 = stmt.executeQuery(sql);
+            rs3.next();
+            String xDeviceId = rs3.getString("CONFIG_VALUE");
+
+            sql = "select * from GAME where GAME_ID = " + gameId;
+            ResultSet rs4 = stmt.executeQuery(sql);
+            rs4.next();
+            long appId = rs4.getLong("APP_ID");
+
+            sql = "select * from SERVER where SERVER_ID = " + serverId;
+            ResultSet rs5 = stmt.executeQuery(sql);
+            rs5.next();
+            String serverHost = rs5.getString("SERVER_HOST");
+
+            System.out.println(authorization);
+            System.out.println(xDeviceId);
+            System.out.println(id);
+            System.out.println(appId);
+            System.out.println(serverHost);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
